116cefe21b7bdb3f5ddd5c116258f11183c99822
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-12-20 00:09:34 +0530

    Update 02_regula falsi method.c

--- Finding Root of Polynomial/02_regula falsi method.c
+++ Finding Root of Polynomial/02_regula falsi method.c
@@ -1,37 +1,42 @@
+// Regula falsi method
 #include<stdio.h>
 #include<math.h>
-float f(float x)
-{
-    return x*x*x-2*x-5; // write function here
+
+#define EPSILON 0.0001 // no of decimal places 
+float func(float a){
+      return a*a*a-2*a-5;
 }
-void regula (float *x, float x0, float x1, float fx0, float fx1, int *itr)
-{
-    *x = x0 - ((x1 - x0) / (fx1 - fx0))*fx0;
-    ++(*itr);
-    printf("Iteration no. %3d X = %7.5f \n", *itr, *x);
+float bisect(float x1,float x2){
+     return (x1*func(x2)-x2*func(x1))/(func(x2)-func(x1));
 }
-int main ()
-{
-    int itr = 0, maxmitr;
-    float x0,x1,x2,x3,allerr;
-    printf("\nEnter the values of x0, x1, allowed error and maximum iterations:\n");
-    scanf("%f %f %f %d", &x0, &x1, &allerr, &maxmitr);
-    regula (&x2, x0, x1, f(x0), f(x1), &itr);
-    do
-    {
-        if (f(x0)*f(x2) < 0)
-            x1=x2;
-        else
-            x0=x2;
-        regula (&x3, x0, x1, f(x0), f(x1), &itr);
-        if (fabs(x3-x2) < allerr)
-        {
-            printf("After %d iterations, root = %6.4f\n", itr, x3);
+
+int main(){
+    int i=1,max_iteration;
+    float x,x1,x2,x3;
+    printf("Enter the value of max iteration ");;
+    scanf("%f",&max_iteration);
+    printf("Enter the value of x1 and x2\n");
+    scanf("%f%f",&x1,&x2);
+    if(func(x1)*func(x2)>0){
+        printf("Invalid values \n");
+        return 0;  // there will some values in iteration whose condition will not 
+    }              // be satisfy so iteration may be less from what you have given
+    x=bisect(x1,x2);
+    do{
+        if(func(x1)*func(x)<0)
+            x2=x;
+        else 
+           x1=x; 
+           printf("Iteration =%d Roots =%f\n",i,x); // it will print each iteration with roots
+        x3=bisect(x1,x2);
+        if(fabs(x3-x)<EPSILON){ 
+            printf("Rooot is %f Total iteration =%d",x,i);
             return 0;
         }
-        x2=x3;
-    }
-    while (itr<maxmitr);
-    printf("Solution does not converge or iterations not sufficient:\n");
-    return 1;
+        x=x3; //imp step
+        i++;
+    }while(i<=max_iteration);
+    printf("Root is %f Total iteratin is %d",x,--i);
+    return 0;
+
 }
