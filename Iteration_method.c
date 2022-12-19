73c60ede9a3e0990e642a3fa00a573d9cfc7d1ff
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-12-20 01:43:05 +0530

    Create Iteration_method.c

---
+++ Iteration_method.c
@@ -0,0 +1,43 @@
+//Iteration method 
+#include<stdio.h>
+#include<math.h>
+#define error 0.0001
+float func(float x){
+    return x*x*x+x*x-1;
+}
+float findat(float x){
+    return 1/sqrt(1+x);  // value of x in function
+}
+float differn(float x){
+    return 1/2.0*(pow(1,pow((1,x),3/2.0)));
+}
+
+int main(){
+    int i=1,max_iteration;
+    float x1,x2,x0,x;
+    printf("Enter the max iteration ");
+    scanf("%d",&max_iteration);
+    printf("Enter the two values of x1 and x2");
+    scanf("%f%f",&x1,&x2);
+    if(func(x1)*func(x2)>0){
+        printf("Invalid values ");
+        return 0;
+    }
+    x0=(x1+x2)/2;
+    if(differn(x0)>1){
+        printf("Function form is not correct Iteratin method can't be applied ");
+        return 0;
+    }
+    while(i<=max_iteration){
+        x=findat(x0);
+        if(fabs(x-x0)<error){
+            printf("Iteration is %d Final root is %f",i,x);
+            return 0;
+        }
+        printf("Iteration =%d Roots =%f\n",i,x);
+        x0=x; // imp step
+        i++;
+    }
+    printf("Root =%f Total iteration =%d",x,--i);
+    return 0;
+}
