93a23b0cc45da8dc1194f4cae507e918b82d16ac
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-12-24 00:04:07 +0530

    Update 03_Newton Raphson Method.c

--- Finding Root of Polynomial/03_Newton Raphson Method.c
+++ Finding Root of Polynomial/03_Newton Raphson Method.c
@@ -1,90 +1,62 @@
 #include<stdio.h>
 #include<math.h>
-#include<stdlib.h>
-
-#define    f(x)    3*x - cos(x) -1  // function of which root to be find
-
-#define   g(x)   3 + sin(x)  // derivation of function
-
-void main()
-{
-	 float x0, x1, f0, f1, g0, e;
-	 int step = 1, N;
-
-	 printf("\nEnter initial guess:\n");
-	 scanf("%f", &x0);
-	 printf("Enter tolerable error:\n");
-	 scanf("%f", &e);
-	 printf("Enter maximum iteration:\n");
-	 scanf("%d", &N);
-	 // Implementing Newton Raphson Method 
-	 printf("\nStep\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
-	 do
-	 {
-		  g0 = g(x0);
-		  f0 = f(x0);
-		  if(g0 == 0.0)
-		  {
-			   printf("Mathematical Error.");
-			   exit(0);
-		  }
-		  x1 = x0 - f0/g0;
-		  printf("%d\t\t%f\t%f\t%f\t%f\n",step,x0,f0,x1,f1);
-		  x0 = x1;
-		  step = step+1;
-		  if(step > N)
-		  {
-			   printf("Not Convergent.");
-			   exit(0);
-		  }
-		  f1 = f(x1);
-		  
-	 }while(fabs(f1)>e);
-	
-	 printf("\nRoot is: %f", x1);
-	 getch();
-}
-
-
-// 2nd method
-//Newton raphson method
-#include<stdio.h>
-#include<math.h>
 #define EPSILON 0.0001
-float func(float x){
-    return x*x*x-3*x-5;
+
+float f(float x)
+{
+    return x*x*x - 3*x -5;
 }
 
-float diff(float x){
-    return 3*x*x*-3;
+float differentiate(float x)
+{
+    return 3*x*x - 3;
 }
 
-int main(){
-    int i=1,max_iteration;
-    float x1,x2,x0,x;
-    printf("Enter the max iteration ");
-    scanf("%d",&max_iteration);
-    printf("Enter the value of x1 and x2 ");
-    scanf("%f%f",&x1,&x2);
-    if(func(x1)*func(x2)>0){
-        printf("Invalid value ");
-        return 0;
-    }
-    //find x0 you can also use x0=(x1+x2)/2
-    if(fabs(x1)<fabs(x2))
-        x0=x1; // take smaller value
-    else 
-        x1=x2;
-    while(i<=max_iteration){
-        x=x0-(func(x0)/diff(x0));
-        if(fabs(x-x0)<EPSILON){
-            printf("Iteration =%d Final Root =%f\n",i,x);
-            return 0;
+int main()
+{
+    int maxIteration,i;
+    float x1,x2,x,x0;
+    printf("Enter Maximum no of Iterations\n");
+    scanf("%d",&maxIteration);
+
+//......Compute x1 and x2............. 
+   
+   do
+   {
+        printf("Enter the value of x1 and x2(starting boundary)");
+        scanf("%f%f",&x1,&x2);
+        if(f(x1)*f(x2)>0)
+        {
+           printf("Boundary Values are Invalid\n");
+           continue;
         }
-        printf("Iteration =%d Roots =%f\n",i,x);
-        x0=x; // imp step 
-        i++;
+        else
+        {
+            printf("Roots Lie between %f and %f\n",x1,x2);
+            break;
+        }
+    } while(1);
+
+    //find x0
+    if(fabs(f(x1)) < fabs(f(x2)))
+        x0 = x1;
+    else
+        x1 = x2;    
+
+    //Apply Successive approximation to find the root b/w x1 and x2
+    //..........Find root............   
+    for(i=1;i<=maxIteration;i++)
+    {
+      x = x0 - (f(x0)/differentiate(x0));  
+      
+      if(fabs(x-x0)<EPSILON)
+      {       
+            printf("Iterations=%d  Final Root=%f\n",i,x);
+           return 0;
+      }      
+      printf("Iterations=%d  Roots=%f\n",i,x); 
+      x0=x;
     }
-    printf("Roots =%f Total iteration =%d ",x,--i);
+    printf("Root=%f  Total Iterations=%d",x,--i);
     return 0;
 }
