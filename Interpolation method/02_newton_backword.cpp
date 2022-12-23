edc8fcca961cc9f83644899e8cb02ca1385b416b
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-12-23 21:31:49 +0530

    Update 02_newton_backword.cpp

--- Interpolation method/02_newton_backword.cpp
+++ Interpolation method/02_newton_backword.cpp
@@ -1,68 +1,54 @@
-// CPP Program to interpolate using
-// newton backward interpolation
-#include <bits/stdc++.h>
-using namespace std;
-
-// Calculation of u mentioned in formula
-float u_cal(float u, int n)
-{
-	float temp = u;
-	for (int i = 1; i < n; i++)
-		temp = temp * (u + i);
-	return temp;
-}
-
-// Calculating factorial of given n
-int fact(int n)
-{
-	int f = 1;
-	for (int i = 2; i <= n; i++)
-		f *= i;
-	return f;
-}
+//Newton Backward Interploation Method
+#include<stdio.h>
+#include<math.h>
 
 int main()
 {
-	// number of values given
-	int n = 5;
-	float x[] = { 1891, 1901, 1911,
-				1921, 1931 };
-				
-	// y[][] is used for difference
-	// table and y[][0] used for input
-	float y[n][n];
-	y[0][0] = 46;
-	y[1][0] = 66;
-	y[2][0] = 81;
-	y[3][0] = 93;
-	y[4][0] = 101;
+    float x,u1,u,y;
+    int i,j,n,fact;
+    printf("Enter no. of terms\n");
+    scanf("%d",&n);
+    float a[n][n+1];
+    printf("Enter Values of X \n");
+    for(i=0;i<n;i++)
+          scanf("%f",&a[i][0]);
+    printf("Enter Values of Y\n");
+    for(i=0;i<n;i++)
+          scanf("%f",&a[i][1]);
+          
+    printf("Enter value of x for which you want y\n");
+    scanf("%f",&x);
 
-	// Calculating the backward difference table
-	for (int i = 1; i < n; i++) {
-		for (int j = n - 1; j >= i; j--)
-			y[j][i] = y[j][i - 1] - y[j - 1][i - 1];
-	}
+    //Find the Difference Table    
+    for(j=2;j<n+1;j++)
+    {
+        for(i=0;i<n-j+1;i++)
+            a[i][j] = a[i+1][j-1]-a[i][j-1];
+    }
 
-	// Displaying the backward difference table
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= i; j++)
-			cout << setw(4) << y[i][j]
-				<< "\t";
-		cout << endl;
-	}
-
-	// Value to interpolate at
-	float value = 1925;
-
-	// Initializing u and sum
-	float sum = y[n - 1][0];
-	float u = (value - x[n - 1]) / (x[1] - x[0]);
-	for (int i = 1; i < n; i++) {
-		sum = sum + (u_cal(u, i) * y[n - 1][i]) /
-									fact(i);
-	}
-
-	cout << "\n Value at " << value << " is "
-		<< sum << endl;
-	return 0;
+    //Print the Difference Table
+    printf("The Difference Table is as follows:\n");
+    for(i=0;i<n;i++)
+    {
+     for(j=0;j<=n-i;j++)
+        printf("%f ",a[i][j]);
+     printf("\n");
+    }
+    
+    //find u
+    u= (x - a[n-1][0])/(a[1][0]-a[0][0]);
+    y=a[n-1][1]; 
+    //Initial k, u1, j
+    u1=u;
+    fact=1;
+    j=2;  //corresponds to column no.
+    for(i=n-2;i>=0;i--)
+    {
+        y=y+(u1*a[i][j])/fact;
+        //Update k, u1, j
+        fact=fact*j; 
+        u1=u1*(u+(j-1));
+        j++;
+    }
+    printf("\n\nValue at X=%g is = %f", x,y);
 }
