cddf03288ffd4b1d498bb1d1108c4f441094b3f8
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2022-12-23 19:36:21 +0530

    Update 01_Newton Forward.c

--- Interpolation method/01_Newton Forward.c
+++ Interpolation method/01_Newton Forward.c
@@ -1,49 +1,42 @@
+//Newton Forward Interploation Method
 #include<stdio.h>
-#define MAXN 100
-#define ORDER 4
+#include<math.h>
 
 int main()
 {
-    
-    float ax[MAXN+1], ay [MAXN+1], diff[MAXN+1][ORDER+1], nr=1.0, dr=1.0,x,p,h,yp;
-    int n,i,j,k;
-    printf("\nEnter the value of n:\n");
+    float x,u1,u,y;
+    int i,j,n,fact;
+    printf("Enter no. of terms\n");
     scanf("%d",&n);
-
-    printf("\nEnter the values in form x,y:\n");
-    for (i=0;i<=n;i++)
-        scanf("%f %f",&ax[i],&ay[i]);
-    printf("\nEnter the value of x for which the value of y is wanted: \n");
+    float a[n][n+1];
+    printf("Enter Values of X\n");
+    for(i=0;i<n;i++)
+          scanf("%f",&a[i][0]);
+    printf("Enter Values of Y\n");
+    for(i=0;i<n;i++)
+          scanf("%f",&a[i][1]); 
+    printf("Enter value of x for which you want y\n");
     scanf("%f",&x);
-    h=ax[1]-ax[0];
 
-    //now making the difference table
-    //calculating the 1st order of differences
-    for (i=0;i<=n-1;i++)
-        diff[i][1] = ay[i+1]-ay[i];
-
-    //now calculating the second and higher order differences
-    for (j=2;j<=ORDER;j++)
-        for(i=0;i<=n-j;i++)
-        diff[i][j] = diff[i+1][j-1] - diff[i][j-1];
-
-    //now finding x0
-    i=0;
-    while (!(ax[i]>x))
-        i++;
-
-    //now ax[i] is x0 and ay[i] is y0
-    i--;
-    p = (x-ax[i])/h;
-    yp = ay[i];
-
-    //now carrying out interpolation
-    for (k=1;k<=ORDER;k++)
+    //Find the Difference Table    
+    for(j=2;j<n+1;j++)
     {
-        nr *=p-k+1;
-        dr *=k;
-        yp +=(nr/dr)*diff[i][k];
+        for(i=0;i<n-j+1;i++)
+            a[i][j] = a[i+1][j-1]-a[i][j-1];
     }
-    printf("\nWhen x = %6.1f, corresponding y = %6.2f\n",x,yp);
-    return 0;
+    
+    //find u
+    u= (x - a[0][0])/(a[1][0]-a[0][0]);
+    //Initial y, u1, k
+    y=a[0][1]; 
+    u1=u;
+    fact=1;
+    for(i=2;i<=n;i++)
+    {
+        y=y+(u1*a[0][i])/fact;
+        //Update k, u1
+        fact=fact*i; 
+        u1=u1*(u-(i-1));
+    }
+    printf("\n\nValue at X=%g is = %f", x,y);
 }
